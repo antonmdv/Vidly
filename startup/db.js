818fb3074c300ebd77e48086fc3ea9fb4cb29eca
Anton <antonmdv1@gmail.com>
2025-06-11 20:02:33 -0700

    add db name

--- startup/db.js
+++ startup/db.js
@@ -3,9 +3,16 @@ const mongoose = require('mongoose')
 const config = require('config')
 
 module.exports = function () {
-    const db = config.get('db')
-    mongoose.connect(db)
-        .then(() => {
-            winston.info(`Connected to ${db}...`)
-        })
+    // const db = config.get('db')
+    // mongoose.connect(db)
+    //     .then(() => {
+    //         winston.info(`Connected to ${db}...`)
+    //     })
+    const uri = config.get('db'); // your atlas string, no /vidly
+    mongoose.connect(uri, {
+        useNewUrlParser:    true,
+        useUnifiedTopology: true,
+        dbName:             'vidly-Anton'     // <-- explicitly choose the DB
+    })
+        .then(() => winston.info(`Connected to dbName=vidly-Anton…`));
 }
